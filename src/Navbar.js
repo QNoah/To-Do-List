19d666df757447f8a4c38bf8a528c2ddff5f0c5e
Quinten <90308786+QNoah@users.noreply.github.com>
2023-01-14 17:46:29 +0100

    1st commit
    - Began with a bit of styling.
    - Navbar has been added (sort of)

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <h1>My-To-Do=List</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/list">View your list</a>
+                <a href="/contact">Contact</a>
+            </div>
+        </nav>
+    );
+}
+
+    export default Navbar;
