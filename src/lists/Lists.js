fc5b5c5ad5dbc1a2da34aeecc1ab2041a95bc128
Quinten <90308786+QNoah@users.noreply.github.com>
2023-01-18 19:49:08 +0100

    Cleaning : Data
    Data from lists has been moved to a seperate file: Data.json

--- src/lists/Lists.js
+++ src/lists/Lists.js
@@ -1,12 +1,10 @@
 import {useState} from 'react'
+import Data from './Data.json'
 
 const delete_image = "./delete_image.png";
 const edit_image = './edit_image.png';
 
 const Lists = () => {
-    const [lists, setList] = useState([
-        { id: 1, title: "My first list", created: "2023-16-1"},
-    ]);
     return (
         <div className="lists">
             <h1>Welcome to your overview page of your lists</h1>
@@ -15,18 +13,21 @@ const Lists = () => {
                 color: 'white'
             }}>Create, Delete or Edit your lists!</p>
             <div className="collection-of-lists">
-                {lists.map((list) => (
-                    <div className="list-preview" key={list.id}>
+                {Data.map( datas => {
+                    return (
+                    <div className="list-preview" key={datas.id}>
                         <h2 style={{
                             color: 'white'
-                        }}>{list.title}</h2>
-                        <p>Created at: {list.created}</p>
+                        }}>{datas.title}</h2>
+                        <p>Created at: {datas.created}</p>
                         <div className="action-images">
                             <img src={edit_image} className="action-images-list" />
                             <img src={delete_image} className="action-images-list" />
                         </div>
                     </div>
-                ))}
+                )
+            })
+        }
                 <div id="create-new">
                     <button type="submit" style={{
                         marginTop: "10px"
